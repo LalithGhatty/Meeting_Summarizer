69d250ceb987c3738824a4facb8622e64951dde8
LalithGhatty <gvsklalith2005@gmail.com>
2025-10-16 20:05:03 +0530

    Ensure vite plugin-react installed and simplify config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  root: '.',
-  server: {
-    port: 3000,
-    proxy:
-      command === 'serve'
-        ? {
-            '/api': {
-              target: 'http://localhost:8000',
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-          }
-        : undefined, // disable proxy during build on Render
-  },
   build: {
     outDir: 'dist',
   },
-}))
+})
+
