ca4c267ed0fa17f46449770d2b1bb6684d14bb1d
LalithGhatty <gvsklalith2005@gmail.com>
2025-10-16 19:28:07 +0530

    Fix ReactDOM render syntax for Vite build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
